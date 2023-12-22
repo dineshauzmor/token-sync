5404fccad8644f3e3db6635064e6197c675c0b4a
Nitesh Seram <niteshseram@gmail.com>
2023-12-22 14:00:58 +0530

    use style dictionary and token studio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import "./../build/css/variables.css";
 
 function App() {
   return (
     <Button>
-      Button
+      <Text>I am a button</Text>
     </Button>
-  )
+  );
 }
 
-export default App
+export default App;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: var(--colorsPrimary950);
+  border-radius: var(--aldo2);
+  border: 1px solid black;
+`;
+
+const Text = styled.span`
+  font: var(--textBaseFontMedium);
+  color: var(--colorsBaseBlack);
+`;
