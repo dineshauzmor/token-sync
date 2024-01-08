16b3da0ffa57c8c14898b87360e2d4704e3e96ee
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-01-08 17:38:29 +0530

    minor fixes

--- src/routes/learn.jsx
+++ src/routes/learn.jsx
@@ -1,7 +1,31 @@
 import { Button } from "auzmorui";
-import "./../../build/css/variables-auzmor1.css";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const Learn = () => {
+  function importStylesheet(url) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `${url}?v=${Date.now()}`; // Append a unique identifier
+    document.head.appendChild(link);
+    return link;
+  }
+  
+  // Function to remove CSS file by removing the corresponding link element
+  function removeStylesheet(linkElement) {
+    if (linkElement && linkElement.parentNode) {
+      linkElement.parentNode.removeChild(linkElement);
+    }
+  }
+  const linkk = useRef()
+  useEffect(() => {
+    linkk.current = importStylesheet("./../../build/css/variables-auzmor1.css")
+  
+    return () => {
+      removeStylesheet(linkk.current)
+    }
+  }, [])
+  
   return (
     <div>
       <Button>I am Learn</Button>
